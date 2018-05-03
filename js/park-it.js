529ffc0814c6fe5acb0d1e253422bb4d202e57f3
Pavel Radchuk <pradchuk2@mail.greenriver.edu>
2018-05-03 14:43:18 -0400

    created the alerts on click

---
+++ js/park-it.js
@@ -0,0 +1,10 @@
+$("#park").click(function () {
+    alert("set parking location");
+});
+
+$("#retrieve").click(function () {
+    alert("get parking location");
+});
+$("#gotIt").click(function () {
+    $("#instructions").hide();
+});
