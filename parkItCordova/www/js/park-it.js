2072028f96537edefbdb0a2d51c84a55353e6785
Pavel Radchuk <pradchuk2@mail.greenriver.edu>
2018-05-10 14:00:25 -0400

    onDeviceReady

--- parkItCordova/www/js/park-it.js
+++ parkItCordova/www/js/park-it.js
@@ -8,3 +8,17 @@ $("#retrieve").click(function () {
 $("#gotIt").click(function () {
     $("#instructions").hide();
 });
+$("document").ready(init);
+function onDiviceReady() {
+
+    //load the correct stylesheet
+    if(cordova.platformid == 'ios'){
+        $('head').append('<link rel="stylesheet" href="css/park-it-ios.css" type="text/css" />');
+        //prevents status bar from overlaying web view
+        window.StatusBar.overlaysWebView(false);
+        window.StatusBar.styleDefult();
+    } else {
+        $('head').append('<link rel="stylesheet" herf="css/park-it-android.css" type="text/css" />');
+        window.StatusBar.backgroundColorByHexString("#1565C0");
+    }
+}
