13a3f782e67478a5e534e44bd51a8a6a8e941e01
Pavel Radchuk <pradchuk2@mail.greenriver.edu>
2018-05-10 16:41:47 -0400

    finished Cordova integration

--- parkItCordova/platforms/browser/www/js/park-it.js
+++ parkItCordova/platforms/browser/www/js/park-it.js
@@ -1,15 +1,27 @@
+var storage;
+
+$("document").ready(init);
+
 $("#park").click(function () {
-    alert("Set parking location");
+    setParkingLocation();
 });
 
 $("#retrieve").click(function () {
     alert("Get parking location");
+    $("#instructions").slideUp();
+    $("#directions").slideUp();
 });
+
 $("#gotIt").click(function () {
-    $("#instructions").hide();
+    $("#instructions").slideUp();
 });
-$("document").ready(init);
-function onDiviceReady() {
+
+function init(){
+    document.addEventListener("deviceready", onDeviceReady,false);
+    storage = window.localStorage;
+}
+
+function onDeviceReady() {
 
     //load the correct stylesheet
     if(cordova.platformid == 'ios'){
@@ -21,4 +33,36 @@ function onDiviceReady() {
         $('head').append('<link rel="stylesheet" herf="css/park-it-android.css" type="text/css" />');
         window.StatusBar.backgroundColorByHexString("#1565C0");
     }
+}
+
+
+
+function setParkingLocation() {
+    navigator.geolocation.getCurrentPosition(setParkingLocationSuccess, setParkingLocationError, { enableHighAccuracy:true });
+}
+
+function setParkingLocationSuccess(position) {
+
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
+    storage.setItem("parkedLatitude", latitude);
+    storage.setItem("parkedLongitude", longitude);
+
+    //Add statement to store the longitude
+    navigator.notification.alert("Parking Location Saved. (Lat: " + latitude + ", Long: " + longitude + ")");
+
+    //Display an alert that shows the latitude and longitude
+    //use navigator.notification.alert(msg)
+
+    showParkingLocation();
+}
+
+function setParkingLocationError(error) {
+    navigator.notification.alert("Error Code: " + error.code + "\nError Message: " + error.message);
+}
+
+function showParkingLocation(){
+    navigator.notification.alert("You are parked at lat: " + storage.getItem("parkedLatitude") + ", long: " + storage.getItem("parkedLongitude"));
+    //hide directions and instructions
+
 }
