56a5e301b72d94cd3b5cd3b072e8a383dfc76922
Sam Ourn <sourn@mail.greenriver.edu>
2018-05-10 13:43:21 -0400

    Created cordova project and transferred existing files

---
+++ parkItCordova/www/js/park-it.js
@@ -0,0 +1,10 @@
+$("#park").click(function () {
+    alert("Set parking location");
+});
+
+$("#retrieve").click(function () {
+    alert("Get parking location");
+});
+$("#gotIt").click(function () {
+    $("#instructions").hide();
+});
